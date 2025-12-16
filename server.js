07bc51af1ebb313dd7518a7c68f48a3c2afb74b4
Luna Pérez Troncoso <lunapereztroncoso2000@gmail.com>
2025-12-16 22:38:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const { createCanvas } = require('canvas');
 const CONFIG = {
     PORT: 3000,
     // Base de datos real en Render
-    DB_URL:'postgresql://database_final_project_user:4jwEikmkqCst9WFPeevZuEhdtjtfIoiu@dpg-d4ttnde3jp1c73f51e9g-a.oregon-postgres.render.com:5432/database_final_project?sslmode=require',                                                                                                                                                                                                                                                                                                        
+    DB_URL:'',                                                                                                                                                                                                                                                                                                        
     OLLAMA_URL: 'http://localhost:11434',
     MODELO: 'qwen3:8b'
 };
@@ -815,4 +815,5 @@ async function crearAPI() {
 crearAPI().catch(error => {
     console.error('Error:', error.message);
     process.exit(1);
+
 });
